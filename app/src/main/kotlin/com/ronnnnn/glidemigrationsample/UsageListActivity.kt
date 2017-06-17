c64fa52341472cb1e602b78f18ad2308ba8c6394
Seiya Kokushi <xxx.xyz-s-.xxx@i.softbank.jp>
2017-06-17 16:25:10 +0900

    initial commit

---
+++ app/src/main/kotlin/com/ronnnnn/glidemigrationsample/UsageListActivity.kt
@@ -0,0 +1,12 @@
+package com.ronnnnn.glidemigrationsample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class UsageListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_usage_list)
+    }
+}
