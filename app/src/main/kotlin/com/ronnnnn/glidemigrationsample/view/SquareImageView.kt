9a0b3a857b450e481134dcede71d8cba808e5b0a
Seiya Kokushi <xxx.xyz-s-.xxx@i.softbank.jp>
2017-06-17 18:36:22 +0900

    develop SquareImageView

---
+++ app/src/main/kotlin/com/ronnnnn/glidemigrationsample/view/SquareImageView.kt
@@ -0,0 +1,21 @@
+package com.ronnnnn.glidemigrationsample.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+/**
+ * Created by kokushiseiya on 2017/06/17.
+ */
+class SquareImageView(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0,
+        defStyleRes: Int = 0) : ImageView(context, attrs, defStyleAttr, defStyleRes) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        setMeasuredDimension(width, measuredWidth)
+    }
+}
