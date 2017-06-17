a6ef7f602eda2794b466bbf9dc86cb98d2aff840
Seiya Kokushi <xxx.xyz-s-.xxx@i.softbank.jp>
2017-06-17 18:15:00 +0900

    develop usage list

---
+++ app/src/main/kotlin/com/ronnnnn/glidemigrationsample/UsageType.kt
@@ -0,0 +1,8 @@
+package com.ronnnnn.glidemigrationsample
+
+/**
+ * Created by kokushiseiya on 2017/06/17.
+ */
+enum class UsageType {
+    BasicUsage
+}
---
+++ app/src/main/kotlin/com/ronnnnn/glidemigrationsample/ViewExtensions.kt
@@ -0,0 +1,16 @@
+package com.ronnnnn.glidemigrationsample
+
+import android.app.Activity
+import android.support.annotation.IdRes
+import android.view.View
+
+/**
+ * Created by kokushiseiya on 2017/06/17.
+ */
+fun <T> View.bindView(@IdRes resourceId: Int): T {
+    return findViewById(resourceId) as T
+}
+
+fun <T> Activity.bindView(@IdRes resourceId: Int): T {
+    return findViewById(resourceId) as T
+}
