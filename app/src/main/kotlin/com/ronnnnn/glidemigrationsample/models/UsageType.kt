289d752856a8bafebf800979dc54501978c2d187
Seiya Kokushi <xxx.xyz-s-.xxx@i.softbank.jp>
2017-06-19 01:20:40 +0900

    add placeholder and error tips

--- app/src/main/kotlin/com/ronnnnn/glidemigrationsample/models/UsageType.kt
+++ app/src/main/kotlin/com/ronnnnn/glidemigrationsample/models/UsageType.kt
@@ -2,6 +2,8 @@ package com.ronnnnn.glidemigrationsample.models
 
 import android.content.Context
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.ronnnnn.glidemigrationsample.R
 import com.ronnnnn.glidemigrationsample.extentions.loadImage
 import java.io.Serializable
 
@@ -9,7 +11,24 @@ import java.io.Serializable
  * Created by kokushiseiya on 2017/06/17.
  */
 enum class UsageType: Serializable {
-    BasicUsage
+    BasicUsage,
+    Placeholder {
+        override fun loadWithGlide(context: Context, imageView: ImageView, imageString: String) {
+            Glide.with(context)
+                    .load(imageString)
+                    .placeholder(R.drawable.image_placeholder)
+                    .into(imageView)
+        }
+    },
+    Error {
+        override fun loadWithGlide(context: Context, imageView: ImageView, imageString: String) {
+            Glide.with(context)
+                    .load(imageString)
+                    .placeholder(R.drawable.image_placeholder)
+                    .error(R.drawable.image_error)
+                    .into(imageView)
+        }
+    }
     ;
 
     open fun loadWithGlide(context: Context, imageView: ImageView, imageString: String) {
