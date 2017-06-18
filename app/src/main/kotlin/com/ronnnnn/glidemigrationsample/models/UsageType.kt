75347f789f0eec01cc976909d5bc3271c1b96c0c
Seiya Kokushi <xxx.xyz-s-.xxx@i.softbank.jp>
2017-06-19 01:22:38 +0900

    remove GlideExtensions

--- app/src/main/kotlin/com/ronnnnn/glidemigrationsample/models/UsageType.kt
+++ app/src/main/kotlin/com/ronnnnn/glidemigrationsample/models/UsageType.kt
@@ -4,13 +4,12 @@ import android.content.Context
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.ronnnnn.glidemigrationsample.R
-import com.ronnnnn.glidemigrationsample.extentions.loadImage
 import java.io.Serializable
 
 /**
  * Created by kokushiseiya on 2017/06/17.
  */
-enum class UsageType: Serializable {
+enum class UsageType : Serializable {
     BasicUsage,
     Placeholder {
         override fun loadWithGlide(context: Context, imageView: ImageView, imageString: String) {
@@ -32,6 +31,8 @@ enum class UsageType: Serializable {
     ;
 
     open fun loadWithGlide(context: Context, imageView: ImageView, imageString: String) {
-        imageView.loadImage(context, imageString)
+        Glide.with(context)
+                .load(imageString)
+                .into(imageView)
     }
 }
